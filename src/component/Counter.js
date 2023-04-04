d4cdfd7f5c73cd8713dbba3423cd97cdb0078047
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-04-04 10:04:45 +0530

    Counter App

---
+++ src/component/Counter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const[value,UseValue]=useState(0);
+
+    const handleInc=()=>{
+        UseValue(value+1);
+    }
+
+    const handeleDec=()=>{
+        UseValue(value-1);
+    }
+  return (
+    <div className='App'>
+       <div>
+       <button className='btn btn-primary' onClick={(event)=>handleInc(event)}>Increment</button>
+        <div className='Value'> 
+        <h1>{value}</h1>
+        </div>
+        <button className='btn btn-primary' onClick={(event)=>handeleDec(event)}>Decrement</button>
+       </div>
+    </div>
+  )
+}
+
+export default Counter
